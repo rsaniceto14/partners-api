28a890db5c11423b32b8f9d261b1ad934a4dcffd
rsaniceto14 <raphaelaniceto14@gmail.com>
2024-11-01 09:44:41 -0300

    [feat] service events configs

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class EventsService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+    return this.prismaService.event.create({
+      data: createEventDto,
+    });
   }
 
   findAll() {
